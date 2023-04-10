984568f14da022d56c8eac611f10e37b7b6b7db8
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-04-10 21:08:44 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,13 +16,9 @@ class MessagesController < ApplicationController
     end
   end
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
